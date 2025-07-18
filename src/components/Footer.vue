2128b7b76a40f32fea81fc02266d3bd5c2c4d4cc
Makarios-Tech101 <ogodogublessing1@gmail.com>
2025-07-18 16:24:39 +0100

    "Home page done"

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,5 +2,185 @@
 </script>
 
 <template>
-    <h1>This is a footer</h1>
+    <div class="footer">
+      <div class="footer-container">
+            <div class="footer-first-column">
+                <p>
+                    At Vnicom Tech Hub, 
+                    we are dedicated to empowering 
+                    the youth with the requisite ICT 
+                    knowledge to jumpstart their careers or 
+                    bring their innovative ideas to life.
+                </p>
+                <div class="footer-first-column-links">
+                    <a href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#f5b800" d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2zm-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93zM6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37z"/>
+                        </svg>
+                    </a>
+                    <a href="#">
+                       <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14"><g fill="none"><g clip-path="url(#primeTwitter0)"><path fill="#f5b800" d="M11.025.656h2.147L8.482 6.03L14 13.344H9.68L6.294 8.909l-3.87 4.435H.275l5.016-5.75L0 .657h4.43L7.486 4.71zm-.755 11.4h1.19L3.78 1.877H2.504z"/></g><defs><clipPath id="primeTwitter0"><path fill="#f5b800" d="M0 0h14v14H0z"/></clipPath></defs></g>
+                     </svg>
+                    </a>
+                    <a href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#f5b800" d="M20.9 2H3.1A1.1 1.1 0 0 0 2 3.1v17.8A1.1 1.1 0 0 0 3.1 22h9.58v-7.75h-2.6v-3h2.6V9a3.64 3.64 0 0 1 3.88-4a20 20 0 0 1 2.33.12v2.7H17.3c-1.26 0-1.5.6-1.5 1.47v1.93h3l-.39 3H15.8V22h5.1a1.1 1.1 0 0 0 1.1-1.1V3.1A1.1 1.1 0 0 0 20.9 2"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            <div class="footer-second-column">
+                <h5>Training</h5>
+                <a href="#">Front-end Development</a>
+                <a href="#">Fullstack Development</a>
+                <a href="#">Python</a>
+                <a href="#">Mobile App Development</a>
+                <a href="#">Data Science</a>
+                <a href="#">Design (UI/UX & Graphic)</a>
+                <a href="#">Computer Literacy</a>
+            </div>
+            <div class="footer-third-column">
+                <h5>Company</h5>
+                <a href="#">About Us</a>
+                <a href="#">Alumni</a>
+                <a href="#">Success Stories</a>
+                <a href="#">Mobile App Development</a>
+                <a href="#">Data Science</a>
+            </div>
+            <div class="footer-fourth-column">
+                <h5>Contact</h5>
+                <div class="footer-fourth-column-contact">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="40px" height="40px" viewBox="0 0 24 24"><path fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m21 3l-6.5 18a.55.55 0 0 1-1 0L10 14l-7-3.5a.55.55 0 0 1 0-1z"/>
+                    </svg>
+                    <p>No 3, Dekunle Atoyebi Street, Oasis Junction, Gasline Road, Magboro Ogun State.</p>
+                </div>
+                <div class="footer-fourth-column-contact">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 16 16"><g fill="#fff"><path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/><path d="M8 14a1 1 0 1 0 0-2a1 1 0 0 0 0 2"/></g>
+                    </svg>
+                    <p>+234 (901) 495 4421</p>
+                </div>
+                <div class="footer-fourth-column-contact">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 16 16"><g fill="#fff"><path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/><path d="M8 14a1 1 0 1 0 0-2a1 1 0 0 0 0 2"/></g>
+                    </svg>
+                    <p>+234 (915) 780 0332</p>
+                </div>
+                <div class="footer-fourth-column-contact">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 16 16"><g fill="#fff"><path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/><path d="M8 14a1 1 0 1 0 0-2a1 1 0 0 0 0 2"/></g>
+                    </svg>
+                    <p>+234 (916) 568 0082</p>
+                </div>
+                <div class="footer-fourth-column-contact">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 24 24"><path fill="#fff" d="M22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2zm-2 0l-8 5l-8-5zm0 12H4V8l8 5l8-5z"/>
+                    </svg>
+                    <p>training@vnicomhub.com</p>
+                </div>
+            </div>
+      </div>
+      <div>
+
+      </div>
+    </div>    
 </template>
+
+
+<style scoped>
+.footer {
+    /* background-color: #f1f2ff; */
+    background-color: #05002D;
+    padding: 70px 0px 40px 0px;
+}
+
+.footer-container {
+    display: flex;
+    width: 90%;
+    margin: 0 auto;
+}
+
+.footer-container h5 {
+    margin: 0px 0px 10px 0px;
+    font-size: 18px;
+    color: #f5b800;
+}
+
+.footer .footer-first-column  {
+   width: 30%;
+   padding-right: 30px;
+
+}
+
+.footer .footer-first-column p {
+    color: #fff;
+    font-size: 13px;
+    line-height: 21px;
+}
+
+.footer .footer-first-column-links {
+    display: flex;
+    align-items: center;
+}
+
+.footer .footer-first-column a {
+   margin-right: 20px;
+   cursor: pointer;
+   
+}
+
+
+.footer .footer-second-column {
+    width: 25%;
+}
+
+
+.footer .footer-second-column a {
+    color:#fff;
+    display: block;
+    text-decoration: none;
+    font-size: 13px;
+    line-height: 30px;
+}
+
+.footer .footer-second-column a:hover {
+    color:#be8e00;
+}
+
+
+
+.footer .footer-third-column {
+    width: 25%;
+    
+}
+
+.footer .footer-third-column a {
+    color:#fff;
+    display: block;
+    text-decoration: none;
+    font-size: 13px;
+    line-height: 30px;
+}
+
+.footer .footer-third-column a:hover {
+    color:#be8e00;
+}
+
+.footer .footer-fourth-column {
+    width: 30%;
+    
+}
+
+.footer .footer-fourth-column .footer-fourth-column-contact {
+   display: flex;
+   align-items: center;
+    
+}
+
+.footer .footer-fourth-column .footer-fourth-column-contact svg {
+ margin-right: 10px;
+ cursor: pointer;
+    
+}
+
+.footer .footer-fourth-column .footer-fourth-column-contact p {
+ color:#fff;
+ font-size: 13px;
+    
+}
+
+</style>
--- src/view/Home.vue
+++ src/view/Home.vue
@@ -288,9 +288,146 @@ onMounted(() => {
                   </div>
                </div>
             </section>
-            
+            <section class="whychoose-section">
+              <div class="whychoose-first-container">
+                <h3>
+                  Empowering You to Build a Tech Career that Thrives
+                </h3>
+                <p>
+                  At Vnicom Tech Hub, we offer industry-focused, hands-on training designed to help beginners and career changers break into tech with confidence. 
+                </p>
+              </div>
+              <div class="whychoose-second-container">
+                <div class="whychoose-second-container-title">
+                  <h3>Accessible Learning. <span class="practical-skill">Practical Skills</span> . Real Growth.</h3>
+                  <p>Our programmes are designed to fit your life and your goals</p>
+                </div>
+                
+                <div class="whychoose-grid">
+                  <div class="whychoose-card" id="whychoose-card-blue">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" viewBox="0 0 12 12"><path fill="#1C3D90" d="M4.25 3.25a1.75 1.75 0 1 1 3.5 0a1.75 1.75 0 0 1-3.5 0m-2 2.25a1.25 1.25 0 1 0 0-2.5a1.25 1.25 0 0 0 0 2.5M11 4.25a1.25 1.25 0 1 1-2.5 0a1.25 1.25 0 0 1 2.5 0M5.25 6C4.56 6 4 6.56 4 7.25V8.5a2 2 0 1 0 4 0V7.25C8 6.56 7.44 6 6.75 6zM3 7.25c0-.289.054-.565.154-.818l-1.231.33a1.25 1.25 0 0 0-.884 1.53l.194.725a2 2 0 0 0 2.45 1.414l.017-.005A3 3 0 0 1 3 8.5zM9 8.5c0 .733-.263 1.405-.7 1.927l.016.004a2 2 0 0 0 2.449-1.414l.194-.725a1.25 1.25 0 0 0-.884-1.53l-1.228-.33c.099.254.153.53.153.818z"/>
+                      </svg>
+                      <h4>Vibrant Support Community</h4>
+                      <p>
+                        Join a community of learners, 
+                        mentors, and industry professionals. 
+                        At Vnicom Tech Hub, you’re never learning alone: 
+                        connect with peers, share ideas, get guidance, 
+                        and grow your network as you build your skills.
+                      </p>
+                  </div>
+                  <div class="whychoose-card" id="whychoose-card-yellow">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" viewBox="0 0 24 24"><path fill="#F5B800" d="m12 11.925l-1.7 1.3q-.15.125-.3.013t-.1-.288l.65-2.1l-1.75-1.4q-.125-.125-.075-.287T8.95 9h2.15l.65-2.05q.05-.175.25-.175t.25.175L12.9 9h2.125q.175 0 .238.163t-.063.287l-1.775 1.4l.65 2.1q.05.175-.1.288t-.3-.013zM12 21l-4.675 1.55q-.5.175-.913-.125t-.412-.8v-6.35q-.95-1.05-1.475-2.4T4 10q0-3.35 2.325-5.675T12 2t5.675 2.325T20 10q0 1.525-.525 2.875T18 15.275v6.35q0 .5-.413.8t-.912.125zm0-5q2.5 0 4.25-1.75T18 10t-1.75-4.25T12 4T7.75 5.75T6 10t1.75 4.25T12 16"/>
+                      </svg>
+                      <h4>Capstone Project</h4>
+                      <p>
+                        Put your skills to the test 
+                        with a real-world capstone project. 
+                        Each programme culminates in a hands-on 
+                        project that challenges you to apply what 
+                        you've learned, solve real problems, 
+                        and build a standout portfolio piece.
+                      </p>
+                  </div>
+                  <div class="whychoose-card" id="whychoose-card-purple">
+                       <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" viewBox="0 0 24 24"><path fill="#281A6A" fill-rule="evenodd" d="M5.4 3h13.2A2.4 2.4 0 0 1 21 5.4v13.2a2.4 2.4 0 0 1-2.4 2.4H5.4A2.4 2.4 0 0 1 3 18.6V5.4A2.4 2.4 0 0 1 5.4 3M9 4h2v5h9v2h-9v9H9v-9H4V9h5z" clip-rule="evenodd"/>
+                      </svg>
+                      <h4>Internship Opportunities</h4>
+                      <p>
+                        Gain industry experience, 
+                        strengthen your resume, 
+                        and transition smoothly 
+                        from the classroom to 
+                        real-world tech environments. 
+                        Our internships are designed to 
+                        help you become job-ready and 
+                        career confident.
+                      </p>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section class="opportunities-section">
+               <div class="opportunities-title">
+                 <h3>Let's Help You Build Your Future</h3>
+                 <p>
+                    At Vnicom Tech Hub, we provide 
+                    best-in-class training, practical 
+                    learning environments, and endless 
+                    opportunities to help you start, grow, 
+                    and excel in your tech career.
+                 </p>
+               </div>
+               <div class="opportunities-grid">
+                 <div class="opportunities-image">
+                    <img src="../../images/grid-img8.jpg">
+                 </div>
+                 <div class="opportunities-content">
+                    <div class="opportunities-grid-items">
+                        <div class="opportunities-grid-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24"><path fill="#F5B800" d="M12 1L3 5v6c0 5.55 3.84 10.74 9 12c5.16-1.26 9-6.45 9-12V5zm3.08 15L12 14.15L8.93 16l.81-3.5l-2.71-2.34l3.58-.31L12 6.55l1.39 3.29l3.58.31l-2.71 2.35z"/>
+                            </svg>
+                        </div>
+                        <div class="opportunities-grid-content">
+                            <h6>Gain a skill</h6>
+                            <p>
+                              Want to earn a new skill or add 
+                              to your existing tech skills? 
+                              join us today at Vnicom Tech Hub 
+                              and discover new possibilities.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="opportunities-grid-items">
+                        <div class="opportunities-grid-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24"><path fill="#F5B800" d="M12 1L3 5v6c0 5.55 3.84 10.74 9 12c5.16-1.26 9-6.45 9-12V5zm3.08 15L12 14.15L8.93 16l.81-3.5l-2.71-2.34l3.58-.31L12 6.55l1.39 3.29l3.58.31l-2.71 2.35z"/>
+                            </svg>
+                        </div>
+                        <div class="opportunities-grid-content">
+                            <h6>Build a career</h6>
+                            <p>
+                              Learn top-rated job focused 
+                              skills in the field of software 
+                              engineering and become employable 
+                              within a year.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="opportunities-grid-items">
+                        <div class="opportunities-grid-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24"><path fill="#F5B800" d="M12 1L3 5v6c0 5.55 3.84 10.74 9 12c5.16-1.26 9-6.45 9-12V5zm3.08 15L12 14.15L8.93 16l.81-3.5l-2.71-2.34l3.58-.31L12 6.55l1.39 3.29l3.58.31l-2.71 2.35z"/>
+                            </svg>
+                        </div>
+                        <div class="opportunities-grid-content">
+                            <h6>Level up your team's skills</h6>
+                            <p>
+                                Have you identified skill gaps 
+                                in your team? Reach out to us 
+                                today to discuss how Vnicom Tech 
+                                Hub can help.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="opportunities-grid-items">
+                        <div class="opportunities-grid-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24"><path fill="#F5B800" d="M12 1L3 5v6c0 5.55 3.84 10.74 9 12c5.16-1.26 9-6.45 9-12V5zm3.08 15L12 14.15L8.93 16l.81-3.5l-2.71-2.34l3.58-.31L12 6.55l1.39 3.29l3.58.31l-2.71 2.35z"/>
+                            </svg>
+                        </div>
+                        <div class="opportunities-grid-content">
+                            <h6>Hire our Grads</h6>
+                            <p>
+                              Looking for talents to help your 
+                              business or startup grow. Reach 
+                              out to us and hire from our amazing 
+                              pool of talents.
+                            </p>
+                        </div>
+                    </div>
+                 </div>
+               </div>
+            </section>
         </main>
-        <Footer />
+      <Footer />
     </div>
 </template>
 
